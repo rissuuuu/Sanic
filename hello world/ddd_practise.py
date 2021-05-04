d0a25ca9f7aaed912fa95f13a3e12d4232460976
Risav Paudel <rissuuuu@gmail.com>
2021-05-04 22:18:55 +0545

    Pdantic dd

--- hello world/ddd_practise.py
+++ hello world/ddd_practise.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ValidationError
 from enum import Enum
+from typing import List
 
 
 class PropertyType(str, Enum):
@@ -13,11 +14,25 @@ class Property(BaseModel):
     lat: float
     lon: float
     type: PropertyType
+    energy_production: float
 
     class Config:
         use_enum_values = True
 
+    def update_availablity(self, subscribed_quantity):
+        self.energy_production -= subscribed_quantity
+
+    def add_to_marketplae(self):
+        marketplace=Marketplace(properties=[self])
+        return marketplace.dict()
+
+
+class Marketplace(BaseModel):
+    properties: List[Property]
+
 
 property = Property(name='Manamaiju', owner="Rishav",
-                    lat=1.2, lon=3.4, type="residential")
-print(property.dict())
+                    lat="1.2", lon=3.4, type="residential", energy_production=120.10)
+# print(property.dict())
+print(property.add_to_marketplae())
+
